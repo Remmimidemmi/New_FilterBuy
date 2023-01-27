050c72503e19c9502cb54510b4901d394e5f35c6
Remimidemmi <bizkitstylle@gmail.com>
2023-01-27 18:25:19 +0300

    Test_commit

--- PageObject/login_page.py
+++ PageObject/login_page.py
@@ -7,9 +7,6 @@ import time
 
 
 class LoginPage(BasePage):
-    def __init__(self):
-        super().__init__(self)
-        self.rand_email, self.rand_password = self.common_user_registration()
 
     def common_user_registration(self):
         reg_email = self.browser.find_element(*LoginPageLocators.SIGN_UP_EMAIL).send_keys(
@@ -21,7 +18,9 @@ class LoginPage(BasePage):
         self.browser.find_element(*LoginPageLocators.LAST_NAME).send_keys(RegistrationCreds.LAST_NAME)
         self.browser.find_element(*LoginPageLocators.SIGN_UP_BUTTON).click()
         self.reg_hello_message_check()
-        return reg_email, reg_password
+        rep_email = reg_email
+        rep_password = reg_password
+        return [rep_email, rep_password]
 
     def common_user_login(self):
         self.browser.find_element(*LoginPageLocators.SiGN_IN_EMAIL).send_keys(LogInCreds.SIGN_IN_EMAIL)
@@ -35,7 +34,7 @@ class LoginPage(BasePage):
         self.browser.find_element(*MainPageLocators.LOGOUT_BUTTON).click()
 
     def repeat_login_after_registration(self):
-        self.common_user_registration()
-        self.browser.find_element(*LoginPageLocators.SiGN_IN_EMAIL).send_keys(self.rand_email)
-        self.browser.find_element(*LoginPageLocators.SIGN_IN_PASSWORD).send_keys(self.rand_password)
+        self.browser.find_element(*LoginPageLocators.SiGN_IN_EMAIL).send_keys()
+        self.browser.find_element(*LoginPageLocators.SIGN_IN_PASSWORD).send_keys()
         self.browser.find_element(*LoginPageLocators.SIGN_IN_BUTTON).click()
+
